b7749a35144aba4dabda62e37347d4fe17a0d33f
majramo <majramo@gmail.com>
2014-09-02 10:27:49 +0200

    requireVisibleXpath is returning webelement and will not run twice in clikc, type ... functions

--- src/test/groovy/verification/dummy/PassedTest.groovy
+++ src/test/groovy/verification/dummy/PassedTest.groovy
@@ -1,12 +1,21 @@
 package verification.dummy
 
 import base.AnyTest
+import org.apache.log4j.Logger
 import org.testng.annotations.Test
 
 public class PassedTest extends AnyTest {
+    public final static Logger log = Logger.getLogger("PT   ")
 
     @Test(groups = "groupOne")
     public void passTestOne() {
+        log.trace("Trace Message!");
+        driver.sleep(100)
+        log.debug("Debug Message!");
+        log.info("Info Message!");
+        log.warn("Warn Message!");
+        log.error("Error Message!");
+        log.fatal("Fatal Message!");
         driver.openUrl("http://www.google.com")
         driver.takeScreenShot("Screen shot")
         tangAssert.assertTrue(true, "Passed test")
