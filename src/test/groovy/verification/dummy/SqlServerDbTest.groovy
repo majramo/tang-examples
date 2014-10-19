4faa2d8db07befde3171708059f8888d38bee4c6
Majid Aram <majramo@gmail.com>
2014-10-19 17:30:36 +0200

    add db icon

--- src/test/groovy/verification/dummy/SqlServerDbTest.groovy
+++ src/test/groovy/verification/dummy/SqlServerDbTest.groovy
@@ -12,13 +12,38 @@ public class SqlServerDbTest extends AnySqlTest {
     @Test(groups = "groupOne", description = "REQ ID = Req abcd")
     public void testOne(ITestContext iTestContext) {
         def query = "SELECT * FROM IkeaSearchPhrases"
-        def dbResult = getDbResult("Get all data from table IkeaSearchPhrases", dbRunTypeRows, query, 0)
+        def dbResult = getDbResult("Get data from table IkeaSearchPhrases", dbRunTypeRows, query, 0)
         int counter = 1
         dbResult.each {
             Reporter.log(counter++ + " " + it.toString())
         }
-        tangAssert.assertTrue(dbResult.size() > 0, "Result should have some value")
+        tangAssert.assertTrue(dbResult.size() > 0, "Result should have some entries")
     }
 
+    @Test(groups = "groupOne", description = "REQ ID = Req abcd")
+    public void testTwo(ITestContext iTestContext) {
+        def query = "SELECT country, phrase, hits, COUNT(*)count FROM IkeaSearchPhrases " +
+                "GROUP BY country, phrase, hits " +
+                "HAVING COUNT(*) > 1"
+        def dbResult = getDbResult("Get data from table IkeaSearchPhrases", dbRunTypeRows, query, 0)
+        int counter = 1
+        dbResult.each {
+            Reporter.log(counter++ + " " + it.toString())
+        }
+        tangAssert.assertTrue(dbResult.size() == 0, "Result should have no entries")
+    }
+
+    @Test(groups = "groupOne", description = "REQ ID = Req abcd")
+    public void testThree(ITestContext iTestContext) {
+        def query = "SELECT country, hits, COUNT(*)count FROM IkeaSearchPhrases " +
+                "GROUP BY country, hits " +
+                "HAVING COUNT(*) > 1"
+        def dbResult = getDbResult("Get data from table IkeaSearchPhrases", dbRunTypeRows, query, 0)
+        int counter = 1
+        dbResult.each {
+            Reporter.log(counter++ + " " + it.toString())
+        }
+        tangAssert.assertTrue(dbResult.size() == 0, "Result should have no entries")
+    }
 
 }
