6dd633707e088cf326d09e06dac971c39128ce84
Majid Aram <majramo@gmail.com>
2015-05-29 09:32:27 +0200

    fixed xpaths

--- src/test/groovy/verification/demo/PassedTest.groovy
+++ src/test/groovy/verification/demo/PassedTest.groovy
@@ -10,12 +10,15 @@ public class PassedTest extends AnyTest {
     @Test(groups = "groupOne", description = "REQ ID = Pass Test")
     public void searchAtGooglePassTest(ITestContext iTestContext) {
         driver.openUrl("http://www.google.se")
-        driver.type("//*[@id='gbqfq']", "Majid Aram")
-        driver.click("//button[@id='gbqfb']")
+        driver.type("lst-ib", "mAJID aRAM")
+        driver.click("//*[@id='sblsbb']/button")
         driver.sleep(500)
         takeScreenshot("")
+        tangAssert.assertTrue(driver.isDisplayed("//h3/a[contains(text(),'Majid Aram')]"), "Check the result contains text")
 
         tangAssert.assertTrue(true, "Passing test method")
+
+
     }
 
 }
